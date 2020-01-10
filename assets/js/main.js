044b7d26fe747c34b5b6cfbdb3c9a0cf79528df3
77ccreed <kiur.jullis@gmail.com>
2020-01-10 12:20:17 +0200

    kõik senine kood

--- assets/js/main.js
+++ assets/js/main.js
@@ -81,22 +81,3 @@
 		}
 
 })(jQuery);
-
-// Map
-var mymap = L.map('mapid').setView([57.803890, 26.061666], 11);
-
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-	maxZoom: 18,
-	id: 'mapbox.streets',
-	accessToken: ''
-}).addTo(mymap);
-
-
-var marker = L.marker([57.803890, 26.061666]).addTo(mymap);
-
-marker.bindPopup("<b>Jaanikese krossirada</b>").openPopup();
-
-var marker = L.marker([57.837509, 26.175690]).addTo(mymap);
-
-marker.bindPopup("<b>Vilaski krossirada</b>").closePopup();
